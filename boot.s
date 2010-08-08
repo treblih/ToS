239be5702949bb72c15fd577f36afebc6852d441
Yang Zhang <treblih.divad@gmail.com>
2010-08-08 20:38:37 +0800

    bootsec done

--- boot.s
+++ boot.s
@@ -1,12 +1,15 @@
-.code16
+.code16	
 .text
-.equ root_offset, 9728 	# 19 * 512
-.equ root_secs, 14
-.equ root_start, 19	# 1 + 8 * 2
-.equ data_start, 33	# 19 + 14
-.equ dir_clus_offset, 0x1a
 
-    jmp main
+.equ	mem_load, 0x7e00 	# 0x7c00 + 0x200
+.equ	root_secs, 14
+.equ	fat_start, 1
+.equ	root_start, 19	# 1 + 8 * 2
+.equ	data_start, 33	# 19 + 14
+.equ	dir_clus_offset, 0x1a
+
+	jmp	start
+	nop
 
 bs_OEMName:		.ascii "treblih "
 bpb_BytesPerSector:	.word 512
@@ -25,121 +28,226 @@ bs_DriveNumber: 	.byte 0
 bs_Unused: 	        .byte 0
 bs_ExtBootSignatur:	.byte 0x29
 bs_SerialNumber:	.long 0xa0a1a2a3
-bs_VolumeLabel: 	.ascii "treblih zy  "
+bs_VolumeLabel: 	.ascii "treblih zy "
 bs_FileSystem: 	        .ascii "FAT12   "
 
-cylinder:	.byte 0
-head:		.byte 0
-sector:		.byte 0
-rootent_loop:	.byte 0	# from 224 to 0
-sec_num:	.word 0
-fat_ent:	.word 0
-loader_name:	.ascii "LOADER  BIN"
-no_loader_str:	.ascii "Loader not found! Panic!"
 
+#------------------------------------------------------------------ 
+# load sector(s) in 3 different situation
+# 1. root secs
+# 2. FAT secs
+# 3. loader.bin
+#------------------------------------------------------------------ 
+start:
+	cli			
+	movw	%cx, %ax
+	movw    %ax, %ds
+	movw    %ax, %es
+	movw    %ax, %gs
+	movw    %ax, %ss
+	movw    $0xffff, %sp
+	sti				
+	
+	# clear screen
+	movw	$0x600, %ax
+	movw	$0x700, %bx
+	xor	%cx, %cx
+	movw	$0x184f, %dx
+	int	$0x10
 
-main:
-    movw $0x7c00, %ax
-    movw %ax, %cs
-    movw %ax, %ds
-    movw %ax, %es
-    movw %ax, %ss
-    movw $0xffff, %sp
+	movw    $msg_search_loader, %si
+	call    print
+	movw    $msg_crlf, %si
+	call    print
 
-reset_floppy:
-    xor %ah, %ah	# func 0 is reset
-    xor %dl, %dl	# drive 0 is floppy disk
-    int $0x13
-    jc reset_floppy	# error happens when CF set, try again
+	# load root secs to 0x7e00
+	movw	$root_start, %ax
+	movw	$root_secs, %cx
+	movw	$mem_load, %bx	
+	call	read_secs
 
-    movw bpb_RootEntries, %cx
-    movw %cx, rootent_loop
-    movw $root_offset, %di
+	# find "LOADER  BIN" in root entries
+	movw 	bpb_RootEntries, %cx
+	movw 	%cx, rootent_loop
+	movw 	$mem_load, %di	# 0x7e00
 find_loader:
-    movw $11, %cx	# max name 11 bytes
-    movw $loader_name, %si
-    cld
-    repe cmpsb
-    je loader_found
-    addw $32, %di
-    subw $1, rootent_loop # not 'dec', as checking CF
-    jnz find_loader
-    jmp no_loader
+	movw 	$11, %cx	# max name 11 bytes
+	movw 	$loader_name, %si
+	cld
+	repe 	cmpsb
+	je 	loader_found
+	andw	$0xfff0, %di	# clear least significant bits of di
+	addw 	$0x20, %di	# + 32, point to the next entry
+	subw 	$1, rootent_loop # not 'dec', as checking CF
+	jnz 	find_loader
+	jmp 	no_loader
 
 loader_found:
-    subw $11, %di	# get to the entry start
-    addw $dir_clus_offset, %di
-    movw (%di), %cx	# LBA
-    movw $0, %ax	# sectors wanted at 1 time
-    movw $0x200, fat_ent # fat is in the 2nd sector
-find_sequence:
-    inc %al
-    call get_fat
-    cmp $0xff7, %cx
-    jl find_sequence
-    call lba2chs
-    movb $2, %ah
-    movb $0, %dl
-    movw $0x200, %bx	# follow the boot sector
-    int $0x13
-    jmp $0x7c00, $0x300
+	movw	$msg_loader_found, %si
+	call	print
+	movw	$msg_crlf, %si
+	call	print
 
-get_fat:		# cx arg/retval
-    pushw %ax
-    pushw %bx
-    pushw %dx
-    movw %cx, %ax
-    subw $2, %ax	# sector offset
-    movw $3, %bx
-    mulw %bx
-    movw $2, %bx
-    div %bx		# least significant bit to CF
-    cmp $0, %dx		# remainder
-    addw $fat_sec, %ax
-    movw (%cx), %dx
-    jnz odd
-    andw 0x0fff, %dx	# even, 0-11
-    movw %dx, %cx
-    jmp end_get_fat
-odd:
-    andw 0xfff0, %dx	# odd, 12-23
-    movw %dx, %cx
-end_get_fat:
-    popw %dx
-    popw %bx
-    popw %ax
-    ret
+	subw	$11, %di	# get to the entry start
+	addw	$dir_clus_offset, %di
+	movw	(%di), %cx
+	movw	%cx, loader_start_clus
 
+	# load FATs to 0x7e00
+	movw	$fat_start, %ax
+	movw	$9, %cx
+	movw    $mem_load, %bx                          
+	call    read_secs
+
+	# load loader.bin to 0050:0000
+	movw    loader_start_clus, %ax	# for next_cluster
+	xor	%dx, %dx
+cluster_sequence:
+	inc	%dx
+	call	next_cluster
+	cmp	$0xff7, %ax
+	jl	cluster_sequence
+
+	movw    loader_start_clus, %ax
+	call    clus2lba                       
+	movw	%dx, %cx	# how many
+	movw	$0x500, %bx	# es:bx 0:500
+	call    read_secs
+
+	movw	$msg_load_done, %si
+	call	print
+	movw	$msg_crlf, %si
+	call	print
+
+	jmp	$0, $0x500
+
+#------------------------------------------------------------------ 
+# int 0x10, func 0xe
+# arg:	si the string
+# 	al hold the char
+#------------------------------------------------------------------ 
+print:
+	lodsb			
+	or	%al, %al	
+	jz	.print_end
+	movb	$0xe, %ah
+	int	$0x10
+	jmp	print
+  .print_end:
+	ret	
+
+#------------------------------------------------------------------ 
+# int 0x13, func 0x2
+# arg:	ax starting sector
+#	cx how many at 1 time
+# 	bx load to es:bx
+#------------------------------------------------------------------ 
+read_secs:
+	movw	$5, %di  	# five retries for error
+  .sectorloop:
+  	pushw	%ax
+	pushw	%bx
+	pushw	%cx
+  	# use ax
+	call    lba2chs                              
+	movw	%cx, %ax
+	movb    $0x02, %ah 	# func 2, now al hold the numbers
+	movb    cylinder, %ch
+	movb    sector, %cl
+	movb    head, %dh
+	movb    $0, %dl		# drive number
+	int     $0x13            
+	jnc     .success        # test for read error
+	dec     %di             
+	popw    %cx
+	popw    %bx
+	popw    %ax
+	jnz     .sectorloop     # read again
+	int     $0x18		# execute Cassette BASIC
+  .success:
+	popw    %cx
+	popw    %bx
+	popw    %ax
+	ret
+
+#------------------------------------------------------------------ 
+# lba = loader_start_clus - 2 + 33
+# arg:	ax
+#------------------------------------------------------------------ 
+clus2lba:
+	subw    $2, %ax  
+	addw    $data_start, %ax
+	ret
+
+#------------------------------------------------------------------ 
+# cylinder  = (logical sector / sectors per track) >> 1
+# head      = (logical sector / sectors per track) & 1
+# sector    = (logical sector MOD sectors per track) + 1
+# arg:	ax LBA
+#------------------------------------------------------------------ 
 lba2chs:
-    pushb %al
-    movw (%di), %ax
-    call clus2lba
-    xor %bx, %bx
-    movb $18, %bl
-    div %bl
-    movb %al, %ch	
-    movb %al, %dh
-    movb %ah, %cl	
-    shrb %ch		# C
-    andb $1, %dh	# H
-    inc %cl		# S
-    popb %al
-    ret
+	pushw	%cx
+	divb	bpb_SectorsPerTrack
+	movb 	%al, %ch	
+	shrb 	%ch		# C
+	movb	%ch, cylinder
+	movb 	%al, %ch
+	andb	$1, %ch		# H
+	movb	%ch, head
+	movb	%ah, %cl	
+	inc 	%cl		# S
+	movb	%cl, sector
+	popw	%cx
+	ret
 
-clus2lba:		# ax arg/retval
-    subw $2, %ax
-    addw $19, %ax
-    ret
+#------------------------------------------------------------------ 
+# search in FAT and find the sequent clusters
+# arg:	ax current cluster
+# ret:	ax the next cluster
+#------------------------------------------------------------------ 
+next_cluster:
+	pushw	%bx
+	pushw	%dx
+	movw	$3, %bx
+	mulw	%bx
+	movw	$2, %bx
+	div	%bx		# least significant bit to CF
+	cmp	$0, %dx		# remainder
+
+	jnz	.odd
+
+	addw	$mem_load, %ax	# 0x7c00 + 0x200 + ax
+	movw	%ax, %bx
+	movw	(%bx), %ax	# 2 bytes
+	andw	0x0fff, %ax	# even, 0-11
+	jmp	.end_next_cluster
+  .odd:
+	addw	$mem_load, %ax	# 0x7c00 + 0x200 + ax
+	movw	%ax, %bx
+	movw	%es:(%bx), %ax	# 2 bytes
+	andw	0xfff0, %ax	# odd, 12-23
+  .end_next_cluster:
+	popw	%dx
+	popw	%bx
+    	ret
 
 no_loader:
-    movw $no_loader_str, %ax
-    movw %ax, %bp
-    movw $24, %cx	# 24 chars
-    movw $0x1301, %ax
-    movw $0xc, %bx
-    movb $0, %dl
-    int  $0x10
-    jmp .
+	movw 	$msg_no_loader, %si
+	call	print
+	jmp	.
 
-.org 510
-.word 0xaa55
+
+sector: 		.byte 0
+head:   		.byte 0
+cylinder:  		.byte 0
+rootent_loop:		.byte 0		# from 224 to 0
+loader_start_clus:     	.word 0
+msg_crlf:     		.long 0x00000a0d
+loader_name:		.asciz "LOADER  BIN"
+msg_search_loader:  	.asciz "Searching Loader"
+msg_loader_found:	.asciz "Loader Found"
+msg_load_done:		.asciz "Load Done. Ready to Jump"
+msg_no_loader:		.asciz "Loader not found! Panic!"
+
+.org 	510
+.word	0xaa55
--- loader.s
+++ loader.s
@@ -1,10 +1,10 @@
 .code16
 .text
-.org 0x100
+# .org 0x100
 
     movw $0xb800, %ax
     movw %ax, %gs
     movb $0xf, %ah
     movb $67, %al
-    movw 80(%gs), %ax
+    movw %ax, %gs:((80 * 0 + 39) * 2)
     jmp .
